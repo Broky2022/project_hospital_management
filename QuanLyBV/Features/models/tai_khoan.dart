7e89e75b75303f240dd014b074baf0bb1dbb161d
Broky2022 <nhktb2k3@gmail.com>
2024-12-10 20:46:11 +0700

    first commit

--- QuanLyBV/Features/models/tai_khoan.dart
+++
@@ -1,9 +0,0 @@
-class TaiKhoan {
-  String _id;
-  String _taiKhoan;
-  String _matKhau;
-
-  TaiKhoan(this._id, this._taiKhoan, this._matKhau);
-
-  String get id => _id;
-}
---
+++ android/app/src/main/kotlin/com/example/project_hospital_management/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project_hospital_management
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
