be84fb49bc9ce1f0e0a855474a09347f82f5af1a
Broky2022 <nhktb2k3@gmail.com>
2024-12-31 17:02:16 +0700

    showData

---
+++ lib/screens/showData.dart
@@ -0,0 +1,36 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+Future<void> showData() async {
+  try {
+    // Mở kết nối đến cơ sở dữ liệu
+    final db = await openDatabase(
+      join(await getDatabasesPath(), 'medical_app.db'),
+    );
+
+    List<Map<String, dynamic>> tables = await db.rawQuery(
+      "SELECT name FROM sqlite_master WHERE type='table' AND name='appointments';", //đổi tên ở đây
+    );
+
+    if (tables.isEmpty) {
+      print("Bảng không tồn tại.");
+      return;
+    }
+
+    List<Map<String, dynamic>> data =
+        await db.query('appointments'); //và ở đây!
+
+    // Kiểm tra dữ liệu và in ra console
+    if (data.isEmpty) {
+      print("Không có dữ liệu trong bảng.");
+    } else {
+      print("Dữ liệu trong bảng:");
+      for (var row in data) {
+        print(row);
+      }
+    }
+  } catch (e) {
+    // Xử lý lỗi
+    print("Lỗi: $e");
+  }
+}
