331367a4e0da791c5b6cce36ba9b8f5dd4b052ce
MinhThuanaz <huutrungho678@gmail.com>
2025-01-03 10:20:14 +0700

    da xoa

--- doc_app/storage/framework/views/addbe896699f2bd3f0cc68122a665912.php
+++
@@ -1,6 +0,0 @@
-<div class="hidden sm:block">
-    <div class="py-8">
-        <div class="border-t border-gray-200"></div>
-    </div>
-</div>
-<?php /**PATH C:\Users\PC\Desktop\project_hospital_management\doc_app\resources\views/components/section-border.blade.php ENDPATH**/ ?>
